beeb7011e8291a34ef9a246b0552e1e0900e1290
Ruben <rcolon85@gmail.com>
2014-09-09 23:49:03 -0400

    config route

--- config/routes.rb
+++ config/routes.rb
@@ -54,3 +54,12 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+SampleApp::Application.routes.draw do
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  .
+  .
+  .
+end
